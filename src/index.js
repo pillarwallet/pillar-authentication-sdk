28d94555b93df1f38867b1de446c448e37e358e2
Rafael Cidade <rafael@2030.io>
2018-04-25 09:46:02 +0100

    bug fix, signature verification

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ exports.sign = (data, privateKey, type = {curve:"secp256k1", hash:"KECCAK256"}) 
     var key = ec.keyFromPrivate(privateKey,"hex");
     var digest  =  hashProvider.hashTable[type.hash](JSON.stringify(data));
     var signature =  ec.sign(digest, key, "hex");
-    return signature.r.toString("hex") + signature.s.toString("hex");
+    return signature = signature.r.toString("hex") + signature.s.toString("hex");
 }
 
 /** 
@@ -41,11 +41,10 @@ exports.verify = (data, publicKey, type = {curve:"secp256k1", hash:"KECCAK256"})
     var ec = curveProvider.curveTable[type.curve]();
     var key = ec.keyFromPublic(publicKey, 'hex');
     var signature = {
-       r : new BN(data.signature.slice(0,64),16),
-       s : new BN(data.signature.slice(64), 16)
+        r: data.signature.slice(0,64),
+        s: data.signature.slice(64)
     }
     delete data['signature'];
     var digest  =  hashProvider.hashTable[type.hash](JSON.stringify(data));
-    return key.verify(digest,signature);
+    return key.verify(digest, signature);
 }
-
